3ec80d57065b19dfe9e0175a382b8254484ff741
HR0620 <195055595+HR0620@users.noreply.github.com>
2026-03-02 22:32:15 +0900

    edit public path

--- src/js/data/exams.js
+++ src/js/data/exams.js
@@ -12,35 +12,14 @@ export const upcoming = {
 
 export const exams = [
     {
-        id: "pre1",
-        number: "第1回",
-        title: "Arcaea共通テスト運用テスト",
-        date: "2025年12月7日実施",
-        description: "共通テスト本番に向けた試行調査．",
-        links: {
-            question: "../../public/assets/pdfs/pre-test_01_q.pdf",
-            answer: "#",
-            form: "https://docs.google.com/forms/d/e/1FAIpQLScAZUsH_C3rxcGZOD08_H70MwCApcLSSthjBH04r_jDqks4iQ/viewform?usp=sharing&ouid=112062388498513339062"  // Google Form link (set to actual URL when available)
-        },
-        stats: {
-            statsDate: "2025.12.10時点",
-            takers: 17,
-            average: 75,
-            stdDev: 14.8,
-            min: 75,
-            median: 75,
-            max: 100
-        }
-    },
-    {
       id: "main1",
       number: "第1回",
       title: "Arcaea共通テスト",
       date: "2026年1月10日実施",
       description: "Arcaea共通テスト",
       links: {
-          question: "../../public/assets/2026/2026_question_01.pdf",
-          answer: "public/assets/2026/2026_answer_01.pdf",
+          question: "assets/2026/2026_question_01.pdf",
+          answer: "assets/2026/2026_answer_01.pdf",
           form: "https://docs.google.com/forms/d/e/1FAIpQLSdbwH0C1dw6ubCxotYqr0XpXJqIVglmb9HaDA46SOMcvZB5KQ/viewform?usp=header"  // Google Form link (set to actual URL when available)
       },
       stats: {
